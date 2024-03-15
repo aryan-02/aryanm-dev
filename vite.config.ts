3f30511b683a563fdb12c798f67eaa31a23fbfe8
Aryan Mediratta <aryan_02@outlook.com>
2024-03-15 09:15:38 -0500

    More config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/aryanm-dev/"
+  base: "https://aryan-02.github.io/aryanm-dev/"
 })
